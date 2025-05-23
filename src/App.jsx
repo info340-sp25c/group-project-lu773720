8bb4d46a3f73c2ea6daad45e928040e855ac3173
Shuaib <shuaibtabit@gmail.com>
2025-05-23 10:48:46 -0700

    Final Card

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,7 @@ import songs from './song.json';
 import '../project-draft/css/style.css';
 
 function App() {
-  return (
-    <div className="card-container">
-      {songs.map((song, idx) => (
-        <Card
-          key={idx}
-          img={song.img}
-          title={song.title}
-          artist={song.artist}
-          description={song.description}
-        />
-      ))}
-    </div>
-  );
+  
 }
 
 export default App;
