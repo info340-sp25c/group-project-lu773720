f2199fa938845fac13a379bf8e762f644543c417
Shuaib <shuaibtabit@gmail.com>
2025-05-23 10:36:19 -0700

    Card Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,23 @@
 
+import React from 'react';
+import Card from './Card';
+import songs from './song.json';
+import '../project-draft/css/style.css';
+
+function App() {
+  return (
+    <div className="card-container">
+      {songs.map((song, idx) => (
+        <Card
+          key={idx}
+          img={song.img}
+          title={song.title}
+          artist={song.artist}
+          description={song.description}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Card.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default function Card({img, title, artist, description}) {
+  return (
+    <div className="card">
+      <img src={img} alt={`${title} album cover art`} />
+      <h1>{title}</h1>
+      <h2>{artist}</h2>
+      <p>{description}</p>
+      <div style={{ flexGrow: 1 }} />
+      <a href="#">
+        <i
+          className="material-icons"
+          style={{ fontSize: '1.5rem', marginRight: '0.5rem', float: 'left' }}
+        >
+          favorite_border
+        </i>
+        <i className="material-icons" style={{ fontSize: '1.5rem' }}>
+          play_arrow
+        </i>{' '}
+        Play
+      </a>
+    </div>
+  );
+}
+
+
